7d1805f72dd8b888be4cf69e27ea8f938d89525d
VanVuong <lai.vanvuong2866@gmail.com>
2021-12-28 09:27:56 +0700

    initial commit

---
+++ src/main/java/com/alpaca/Alpaca_Mock_Project/AlpacaMockProjectApplication.java
@@ -0,0 +1,13 @@
+package com.alpaca.Alpaca_Mock_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpacaMockProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlpacaMockProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alpaca/Alpaca_Mock_Project/AlpacaMockProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpaca.Alpaca_Mock_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlpacaMockProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
